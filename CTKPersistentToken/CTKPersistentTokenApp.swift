0dd2ea8b9f4bd98457fa56e9ccae9024e46f8b53
Brandon Weeks <bweeks@google.com>
2021-07-28 13:29:13 -0700

    Initial Commit

---
+++ CTKPersistentToken/CTKPersistentTokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTKPersistentTokenApp.swift
+//  CTKPersistentToken
+//
+//  Created by Brandon Weeks on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CTKPersistentTokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CTKPersistentToken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CTKPersistentToken
+//
+//  Created by Brandon Weeks on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
